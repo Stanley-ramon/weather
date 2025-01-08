016203737a94c6e6d31233f6bc18089e446fad80
Estanlei Sousa <estanlei2020@gmail.com>
2025-01-07 21:46:20 -0300

    colocando api

--- scripts.js
+++ scripts.js
@@ -4,3 +4,26 @@
 
 
 const key = "95015cff1d7bea613144c4393e7577a1"
+
+function dadosonscreen (dados) {
+
+    console.log(dados)
+
+     document.querySelector(".container-weather__city").innerHTML = " Tempo em " + dados.name
+     document.querySelector("container-weather__temp").innerHTML = dados.
+     main.temp
+     document.querySelector("prevision-text").innerHTML = dados.weather[0].description
+}
+
+async function searchcity (city) {
+
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&unicts=metric`).then( response => response.json())
+ 
+    dadosonscreen(dados)
+}
+
+function clickonbutton () {
+     const city = document.querySelector(".input-city").value
+
+    searchcity(city)
+}
