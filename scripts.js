99336716843b8e8c5f6befba6202c4d762681e49
Estanlei Sousa <estanlei2020@gmail.com>
2025-01-08 21:39:14 -0300

    finalizando

--- scripts.js
+++ scripts.js
@@ -6,23 +6,25 @@
 const key = "95015cff1d7bea613144c4393e7577a1"
 
 function dadosonscreen (dados) {
-
     console.log(dados)
 
      document.querySelector(".container-weather__city").innerHTML = " Tempo em " + dados.name
-     document.querySelector("container-weather__temp").innerHTML = dados.
-     main.temp
-     document.querySelector("prevision-text").innerHTML = dados.weather[0].description
+     document.querySelector(".container-weather__temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+     document.querySelector(".prevision-text").innerHTML = dados.weather[0].description
+     document.querySelector(".dampness").innerHTML = " umidade " + dados.main.humidity + "%"
+     document.querySelector(".img-prevision").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+
 }
 
 async function searchcity (city) {
 
-    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&unicts=metric`).then( response => response.json())
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`).then( response => response.json())
  
     dadosonscreen(dados)
 }
 
 function clickonbutton () {
+
      const city = document.querySelector(".input-city").value
 
     searchcity(city)
